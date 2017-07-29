6715a3a495a40133e6d71ffc10196661058dbcf9
沈 <@DESKTOP-LU5I4A9>
2017-07-29 23:08:52 +0800

    Pyhon'Test

---
+++ Translate/Translate.py
@@ -0,0 +1,15 @@
+def Describe_pet(animal_type,pet_name):
+   # ""��ʾ������Ϣ""
+    print(animal_type)
+    print(pet_name)
+Describe_pet("ssss","ddddd")
+
+
+
+
+def Describe_Dog(animal_type,Dog_name = "Harry"):
+   # ""��ʾ������Ϣ""
+    print(animal_type)
+    print(Dog_name)
+Describe_Dog("ssss")
+Describe_Dog("ssss",'Carry ')
